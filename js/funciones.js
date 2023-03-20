6515d35558482bca93f664d2a3bf1c9d8dce1045
ronald manza <ronaldmanza1@g.mail.com>
2023-03-20 16:59:35 -0500

    actualizado todo

---
+++ js/funciones.js
@@ -0,0 +1,11 @@
+function presentacion(){
+    alert("enviar mansaje");
+}
+function saludar(){
+    alert("te saluda ronald manza, gracias por tu interes");
+
+}
+
+const botonmansaje = document.getElementById("mensaje");
+
+botonmensaje.addEventListener("click",mensaje);
